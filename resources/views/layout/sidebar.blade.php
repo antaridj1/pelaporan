28fe8789b5905fdc5274a4a6530debee0f4904f0
Antari <antari.appkey@gmail.com>
2022-12-15 16:15:30 +0800

    fixing

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -3,13 +3,13 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link" href="{{route('home')}}">
+        <a class="nav-link {{Request::is('home') || Request::is('profile')? '' : 'collapsed'}}" href="{{route('home')}}">
           <i class="bi bi-house-door"></i>
           <span>Home</span>
         </a>
       </li><!-- End Dashboard Nav -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{route('laporan.index')}}">
+        <a class="nav-link {{Request::is('laporan')? '' : 'collapsed'}}" href="{{route('laporan.index')}}">
           <i class="bi bi-journal-text"></i>
           <span>Laporan</span>
         </a>
@@ -17,7 +17,7 @@
 
       @if (auth()->user()->role === 'admin')
           <li class="nav-item">
-            <a class="nav-link collapsed" href="{{route('laporan.create')}}">
+            <a class="nav-link {{Request::is('laporan/create')? '' : 'collapsed'}}" href="{{route('laporan.create')}}">
               <i class="bi bi-pencil"></i>
               <span>Buat Laporan</span>
             </a>
@@ -25,14 +25,14 @@
 
       @elseif (auth()->user()->role === 'master_admin')
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{route('pegawai.index')}}">
+        <a class="nav-link {{Request::is('pegawai')? '' : 'collapsed'}}" href="{{route('pegawai.index')}}">
           <i class="bi bi-people"></i>
           <span>Pegawai</span>
         </a>
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{route('unit.index')}}">
+        <a class="nav-link {{Request::is('unit')? '' : 'collapsed'}}" href="{{route('unit.index')}}">
           <i class="bi bi-bank"></i>
           <span>Unit BRI</span>
         </a>
